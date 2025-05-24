07e727e08286bd474b7ac8d87217212ace0ab47f
oricardorico <155673350+oricardorico@users.noreply.github.com>
2025-05-24 03:26:33 -0300

    Update sendPurchase.js

--- api/sendPurchase.js
+++ api/sendPurchase.js
@@ -11,14 +11,18 @@ module.exports = async (req, res) => {
   const ACCESS_TOKEN = "EAARuZCF4UqWgBO9qjbHC8JR4DhgaldsyZCTJTncKbf3JfHIgRO49zEUOlxfRQD1nO7b0YG5j3ZCfwdY6rFaHGNDt18FLuu7WbGE2H5ZAsD172ApNGos4fQqNZCCEHoEQ8iciVT3ZAj7xhcq1Tx9Patlo4aTQ4Fg0g6Jff0QhtGkZB0hJLvZB5s0AAKr7qIZAZCdwZDZD";
 
   try {
-    const { customer, purchase } = req.body;
+    const { Customer, Commissions } = req.body;
 
-    if (!customer?.email || !purchase?.value) {
-      return res.status(400).json({ success: false, error: "Parâmetros 'customer.email' e 'purchase.value' são obrigatórios." });
+    if (!Customer?.email || !Commissions?.charge_amount) {
+      return res.status(400).json({
+        success: false,
+        error: "Parâmetros 'Customer.email' e 'Commissions.charge_amount' são obrigatórios."
+      });
     }
 
-    const email = customer.email;
-    const value = parseFloat(purchase.value);
+    const email = Customer.email;
+    const valueCents = parseFloat(Commissions.charge_amount); // vem em centavos
+    const valueReais = valueCents / 100;
 
     const hashedEmail = crypto.createHash("sha256").update(email.trim().toLowerCase()).digest("hex");
     const timestamp = Math.floor(Date.now() / 1000);
@@ -35,7 +39,7 @@ module.exports = async (req, res) => {
           },
           custom_data: {
             currency: "BRL",
-            value: value
+            value: valueReais
           }
         }
       ]
